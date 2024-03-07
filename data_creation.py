5276519fb46555c3b3832c18cee43eb98b99ac4e
Zhukov Dmitry <146352203+stud0000228135@users.noreply.github.com>
2024-03-07 19:55:53 +0500

    Update data_creation.py

--- data_creation.py
+++ data_creation.py
@@ -12,23 +12,23 @@ if not os.path.exists('test'):
 # Генерируем данные для тренировочного и тестового наборов
 def generate_data(start_date, end_date, num_points, noise_level):
     dates = [start_date + timedelta(days=i) for i in range(num_points)]
-    values = [random.uniform(50, 100) for _ in range(num_points)]
+    values = [random.uniform(80, 100) for _ in range(num_points)]
     noisy_values = [v + random.uniform(-noise_level, noise_level) for v in values]
     return pd.DataFrame({'Date': dates, 'Value': noisy_values})
 
 # Параметры генерации данных
 train_data_params = [
-    (datetime(2010, 1, 1), datetime(2011, 12, 31), 730, 0.5),
-    (datetime(2012, 1, 1), datetime(2013, 12, 31), 731, 0.4),
-    (datetime(2014, 1, 1), datetime(2015, 12, 31), 730, 0.6),
-    (datetime(2016, 1, 1), datetime(2017, 12, 31), 731, 0.3),
-    (datetime(2018, 1, 1), datetime(2019, 12, 31), 730, 0.7)
+    (datetime(2008, 1, 1), datetime(2009, 12, 31), 730, 10),
+    (datetime(2010, 1, 1), datetime(2011, 12, 31), 731, 10),
+    (datetime(2012, 1, 1), datetime(2013, 12, 31), 730, 10),
+    (datetime(2014, 1, 1), datetime(2015, 12, 31), 731, 10),
+    (datetime(2016, 1, 1), datetime(2017, 12, 31), 730, 10)
 ]
 
 test_data_params = [
-    (datetime(2020, 1, 1), datetime(2021, 12, 31), 731, 0.5),
-    (datetime(2022, 1, 1), datetime(2023, 12, 31), 730, 0.4),
-    (datetime(2024, 1, 1), datetime(2025, 12, 31), 731, 0.6)
+    (datetime(2018, 1, 1), datetime(2019, 12, 31), 731, 10),
+    (datetime(2020, 1, 1), datetime(2021, 12, 31), 730, 10),
+    (datetime(2022, 1, 1), datetime(2023, 12, 31), 731, 10)
 ]
 
 # Создаем и сохраняем данные для тренировочного набора
